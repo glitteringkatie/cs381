4ebf0ee4ce03c94ceccbcf92ba08cd2746a1dfae
Kate Hughes <katelhughes93@gmail.com>
2016-01-20 21:20:48 -0800

    Start data definition. Possibly done with defs?

---
+++ hw2/MiniLogo.hugheska.hs
@@ -0,0 +1,32 @@
+-- num   ::= any natural number               -> This can be num or int
+-- var   ::= any variable name                -> This can be a string
+-- macro ::= any macro name
+-- prog  ::= epsilon | cmd; prog              -> sequence of commands
+-- mode  ::= `down` | `up`                    -> pen status
+-- expr  ::= var                              -> variable reference
+--         | num                              -> literal number
+--         | expr + expr                      -> additional expression
+-- cmd   ::= `pen` mode                       -> change pen mode
+--         | `move` (expr, expr)              -> move pen to a new position
+--         | `define` macro ( var* ) { prog } -> define a macro
+--         | `call` macro ( expr* )           -> invoke a macro
+
+import Prelude hiding (Num)
+
+type Num   = Int
+type Var   = String
+type Macro = String
+type Prog  = [Cmd] -- sequence of cmd
+
+data Mode = Up | Down
+     deriving (Eq, Show)
+
+data Expr = Var
+          | Num
+          | Expr Expr
+     deriving Show
+
+data Cmd  = Pen Mode
+          | Move Expr Expr
+          | Define Macro Var Prog
+     deriving Show
