be7c02f4cfed15f75ec72ec334405c5c1bcff6aa
Kate Hughes <katelhughes93@gmail.com>
2016-02-08 21:59:16 -0800

    Utilize template file

--- hw3/HW3.hugheska.hs
+++ hw3/HW3.hugheska.hs
@@ -1,19 +1,80 @@
+module HW3 where
+
 import MiniMiniLogo
 import Render
 
-type State  = Point
-type Domain = State -> (State, Maybe Line)
 
+--
+-- * Semantics of MiniMiniLogo
+--
+
+-- NOTE:
+--   * MiniMiniLogo.hs defines the abstract syntax of MiniMiniLogo and some
+--     functions for generating MiniMiniLogo programs. It contains the type
+--     definitions for Mode, Cmd, and Prog.
+--   * Render.hs contains code for rendering the output of a MiniMiniLogo
+--     program in HTML5. It contains the types definitions for Point and Line.
+
+-- | A type to represent the current state of the pen.
+type State = (Mode,Point)
+
+-- | The initial state of the pen.
+start :: State
+start = (Up,(0,0))
+
+-- | A function that renders the image to HTML. Only works after you have
+--   implemented `prog`. Applying `draw` to a MiniMiniLogo program will
+--   produce an HTML file named MiniMiniLogo.html, which you can load in
+--   your browswer to view the rendered image.
+draw :: Prog -> IO ()
+draw p = let (_,ls) = prog p start in toHTML ls
+
+
+-- Semantic domains:
+--   * Cmd:  State -> (State, Maybe Line)
+--   * Prog: State -> (State, [Line])
+
+
+-- | Semantic function for Cmd.
+--
+--   >>> cmd (Pen Down) (Up,(2,3))
+--   ((Down,(2,3)),Nothing)
+--
+--   >>> cmd (Pen Up) (Down,(2,3))
+--   ((Up,(2,3)),Nothing)
+--
+--   >>> cmd (Move 4 5) (Up,(2,3))
+--   ((Up,(4,5)),Nothing)
+--
+--   >>> cmd (Move 4 5) (Down,(2,3))
+--   ((Down,(4,5)),Just ((2,3),(4,5)))
+--
 cmd :: Cmd -> State -> (State, Maybe Line)
-cmd (Pen m)    (x,y) = undefined
-cmd (Move i j) (x,y) =
+cmd (Pen i)    (m, p)     = (i,p)
+cmd (Move i j) (m, (x,y)) =
+-- change state's mode to up or down and what about the maybe line? something
+--    with p probably -> line if down?
+-- move won't adjust m but it will adjust the point and will return a line if
+--    p is down otherwise probably nothing
 
+
+-- | Semantic function for Prog.
+--
+--   >>> prog (nix 10 10 5 7) start
+--   ((Down,(15,10)),[((10,10),(15,17)),((10,17),(15,10))])
+--
+--   >>> prog (steps 2 0 0) start
+--   ((Down,(2,2)),[((0,0),(0,1)),((0,1),(1,1)),((1,1),(1,2)),((1,2),(2,2))])
 prog :: Prog -> State -> (State, [Line])
 prog []     s = (s,[])
 prog (p:ps) s = move ps (cmd s p)
 
 
--- somewhere we will have to use toHTML to render this all
--- I think this will be the `draw` command referenced in the assignment
--- description. It will take the [Line] from prog since toHTML takes an array
--- of lines
+--
+-- * Extra credit
+--
+
+-- | This should be a MiniMiniLogo program that draws an amazing picture.
+--   Add as many helper functions as you want.
+amazing :: Prog
+amazing = undefined
