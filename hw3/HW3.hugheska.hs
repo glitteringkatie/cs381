5f8e43e254f10cf7c2afea33bede0ffc1249c789
Kate Hughes <katelhughes93@gmail.com>
2016-02-10 17:07:27 -0800

    Fix whitespace and enforce 80 char lines

--- hw3/HW3.hugheska.hs
+++ hw3/HW3.hugheska.hs
@@ -81,18 +81,22 @@ prog (p:ps) s = let res = (cmd p s); line = snd res; state = fst res in
 -- | This should be a MiniMiniLogo program that draws an amazing picture.
 --   Add as many helper functions as you want.
 amazing :: Prog
-amazing = ((Pen Up, Move (14,24), Pen Down, Move (22,26), Move (23,26), Move (21,25), Move (21,24),
-	            Move (22,24), Move (21,23), Move (20,24), Move (14,21), Move (18,11), Move (18,9), 
-	            Move (16,6), Move (16,2), Move (12,2), Move (14,3), Move (12,5), Move (12,6), 
-	            Move (13,9), Move (10,6), Move (9,3), Move (9,2), Move (5,2), Move (7,3), Move (5,6),
-	            Move (5,7), Move (6,9), Move (5,11), Move (5,12), Move (2,15), Move (2,18), Move (3,21), 
-	            Move (2,23), Move (4,25), Move (4,24), Move (3,23), Move (4,22), Move (4,23), Move (5,22), Move (4,21), Move (5,20), 
-	            Move (6,21), Move (7,21), Move (14,23), Move (14,25), Move (13,25), Move (7,22), Move (7,14), Move (6,12), Move (6,22),
-	            Move (7,22), Move (4,28), Move (9,28), Move (9,29), Move (8,28), Move (8,29), Move (7,29), Move (6,30), Move (8,30), 
-	            Move (7,31), Move (13,32), Move (13,31), Move (14,31), Move (13,30), Move (13,29), Move (14,30), Move (15,29), 
-	            Move (15,27), Move (14,26), Move (13,27), Move (13,25))
-                
-	   
+amazing = [(Pen Up, Move (14,24), Pen Down, Move (22,26), Move (23,26),
+              Move (21,25), Move (21,24), Move (22,24), Move (21,23),
+              Move (20,24), Move (14,21), Move (18,11), Move (18,9),
+              Move (16,6), Move (16,2), Move (12,2),
+              Move (14,3), Move (12,5), Move (12,6), Move (13,9), Move (10,6),
+              Move (9,3), Move (9,2), Move (5,2), Move (7,3), Move (5,6),
+              Move (5,7), Move (6,9), Move (5,11), Move (5,12), Move (2,15),
+              Move (2,18), Move (3,21), Move (2,23), Move (4,25), Move (4,24),
+              Move (3,23), Move (4,22), Move (4,23), Move (5,22), Move (4,21),
+              Move (5,20), Move (6,21), Move (7,21), Move (14,23), Move (14,25),
+              Move (13,25), Move (7,22), Move (7,14), Move (6,12), Move (6,22),
+              Move (7,22), Move (4,28), Move (9,28), Move (9,29), Move (8,28),
+              Move (8,29), Move (7,29), Move (6,30), Move (8,30), Move (7,31),
+              Move (13,32), Move (13,31), Move (14,31), Move (13,30),
+              Move (13,29), Move (14,30), Move (15,29), Move (15,27),
+              Move (14,26), Move (13,27), Move (13,25)]
 
 
 
