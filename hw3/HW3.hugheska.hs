d500e5ae2fa331bb6ceaecbea4c8f1de1f271eb1
Andrea Baldwin <baldwina@onid.oregonstate.edu>
2016-02-10 17:04:47 -0800

    yogi maybe done?

--- hw3/HW3.hugheska.hs
+++ hw3/HW3.hugheska.hs
@@ -81,7 +81,7 @@ prog (p:ps) s = let res = (cmd p s); line = snd res; state = fst res in
 -- | This should be a MiniMiniLogo program that draws an amazing picture.
 --   Add as many helper functions as you want.
 amazing :: Prog
-amazing = ((Pen Up, Move (14,24), Pen Down, Move (22,26), Move (23,26), Move (21,25), Move (21,24),
+amazing = [(Pen Up, Move (14,24), Pen Down, Move (22,26), Move (23,26), Move (21,25), Move (21,24),
 	            Move (22,24), Move (21,23), Move (20,24), Move (14,21), Move (18,11), Move (18,9), 
 	            Move (16,6), Move (16,2), Move (12,2), Move (14,3), Move (12,5), Move (12,6), 
 	            Move (13,9), Move (10,6), Move (9,3), Move (9,2), Move (5,2), Move (7,3), Move (5,6),
@@ -90,7 +90,9 @@ amazing = ((Pen Up, Move (14,24), Pen Down, Move (22,26), Move (23,26), Move (21
 	            Move (6,21), Move (7,21), Move (14,23), Move (14,25), Move (13,25), Move (7,22), Move (7,14), Move (6,12), Move (6,22),
 	            Move (7,22), Move (4,28), Move (9,28), Move (9,29), Move (8,28), Move (8,29), Move (7,29), Move (6,30), Move (8,30), 
 	            Move (7,31), Move (13,32), Move (13,31), Move (14,31), Move (13,30), Move (13,29), Move (14,30), Move (15,29), 
-	            Move (15,27), Move (14,26), Move (13,27), Move (13,25))
+	            Move (15,27), Move (14,26), Move (13,27), Move (13,25) Pen Up, Move (8,29) , Pen Down,
+	            Move (13,30) Pen Up, Move (6,24) Pen Down, Move (8,24), Move (9,25) Pen Up, Move (13,28),
+	            Pen Down, Move (14,29), Move (14,27) ,Move (13,28)]
                 
 	   
 
