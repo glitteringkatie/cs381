15e42e194e16f314c396e27c4e5a455f08f8a711
Kate Hughes <katelhughes93@gmail.com>
2016-02-24 16:05:21 -0800

    Make cosmetic changes and add our names

--- HW4/KarelSemantics.hs
+++ HW4/KarelSemantics.hs
@@ -1,3 +1,7 @@
+-- Katie Hughes
+-- Andrea Baldwin
+-- Mikky Cecil
+
 module KarelSemantics where
 
 import Prelude hiding (Either(..))
@@ -20,32 +24,32 @@ stmt :: Stmt -> Defs -> World -> Robot -> Result
 stmt Shutdown   _ _ r = Done r
 stmt Move       _ w r = let f = getFacing r; p = getPos r; n = (neighbor f p)
                         in case isClear n w of
-                             True  -> OK w (setPos n r)
-                             False -> Error ("Blocked at: " ++ show n)
+                          True  -> OK w (setPos n r)
+                          False -> Error ("Blocked at: " ++ show n)
 stmt PickBeeper _ w r = let p = getPos r
                         in if hasBeeper p w
-                              then OK (decBeeper p w) (incBag r)
-                              else Error ("No beeper to pick at: " ++ show p)
+                          then OK (decBeeper p w) (incBag r)
+                          else Error ("No beeper to pick at: " ++ show p)
 --Note: there can be more than one beeper in a spot.
 --We don't need to check if it's a wall because the robot can't walk to a wall spot.
 stmt PutBeeper     _ w r = let p = getPos r
                            in case isEmpty r of
-                                True  -> Error ("No beeper to put.")
-                                False -> OK (incBeeper p w) (decBag r)
+                             True  -> Error ("No beeper to put.")
+                             False -> OK (incBeeper p w) (decBag r)
 stmt (Turn d)      _ w r = let f = getFacing r
                            in OK w (setFacing (cardTurn d f) r)
 stmt (Block ss)    d w r = stmts ss d w r
-stmt (If c x y)    d w r = if test c w r == True 
+stmt (If c x y)    d w r = if test c w r == True
                            then stmt x d w r
                            else stmt y d w r
 stmt (Call m)      d w r = case lookup m d of
                              Just s  -> stmt s d w r
                              Nothing -> Error ("Undefined macro: " ++ m)
-stmt (Iterate i s) d w r = if i > 0 
+stmt (Iterate i s) d w r = if i > 0
                            then case stmt s d w r of
-                                  OK nw nr -> stmt (Iterate (i - 1) s) d nw nr
-                                  Done  nr -> Done nr
-                                  Error s  -> Error s
+                             OK nw nr -> stmt (Iterate (i - 1) s) d nw nr
+                             Done  nr -> Done nr
+                             Error s  -> Error s
                            else OK w r
 stmt _ _ _ _ = undefined
 
